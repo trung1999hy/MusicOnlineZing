67f8eedf894ae58ad04b65f84eee3efbd0df0837
NamTH <anhnam2k.yn@gmail.com>
2023-05-18 22:10:24 +0700

    add permission notificaticons

--- app/src/main/java/com/trungtv/Onmuzik/SplashActivity.kt
+++ app/src/main/java/com/trungtv/Onmuzik/SplashActivity.kt
@@ -1,20 +1,65 @@
 package com.trungtv.Onmuzik
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.android.billingclient.api.*
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.android.billingclient.api.BillingClient
+import com.android.billingclient.api.BillingClientStateListener
+import com.android.billingclient.api.BillingResult
+import com.android.billingclient.api.ConsumeParams
+import com.android.billingclient.api.ConsumeResponseListener
+import com.android.billingclient.api.Purchase
+import com.android.billingclient.api.QueryPurchasesParams
 
 class SplashActivity : AppCompatActivity() {
+
     var billingClient: BillingClient? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         checkInAppPurchase()
+        if (Build.VERSION.SDK_INT >= 33) {
+            if (shouldShowRequestPermissionRationale(android.Manifest.permission.POST_NOTIFICATIONS)) {
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(
+                        android.Manifest.permission.POST_NOTIFICATIONS
+                    ), 200
+                )
+            } else if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ) == PackageManager.PERMISSION_GRANTED
+            )
+                startMain()
+            else ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    android.Manifest.permission.POST_NOTIFICATIONS
+                ), 200
+            )
+        } else {
+            startMain()
+        }
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         startMain()
+
     }
 
     private fun startMain() {
